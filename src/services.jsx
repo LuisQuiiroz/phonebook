8d98abf8b71b79bada87ef65840e664a0c50417b
Luis Quiroz <luisdev99@gmail.com>
2022-06-16 10:03:17 -0500

    proxy config

--- src/services.jsx
+++ src/services.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const baseUrl = 'http://localhost:3001/api/persons';
+// const baseUrl = 'https://polar-mountain-19321.herokuapp.com/api/persons';
+const baseUrl = '/api/persons';
 
 export const getAll = () => {
     const request = axios.get(baseUrl)
